021071927ad0699857153b70351546c78cf14a5b
Zachary Jones <zak@newalexandria.org>
2016-07-17 22:07:22 -0400

    added evac script for macs

---
+++ mac/evac.sh
@@ -0,0 +1,25 @@
+rm -Rf ~/../equinox/
+rm -Rf ~/.dotfiles
+rm -Rf ~/.vim.ana
+rm -Rf ~/images
+rm -Rf ~/Movies/*
+rm -Rf ~/Pictures/*
+rm -Rf ~/Music/*
+rm -Rf ~/Documents/*
+rm -Rf ~/Desktop/*
+rm -Rf ~/Downloads/*
+rm -Rf ~/Library/Application\ Support/MobileSync
+rm -Rf ~/Library/Application\ Support/Adobe 
+rm -Rf ~/Library/LaunchAgents
+rm -Rf ~/Library/Safari
+rm -Rf ~/Library/Keychains
+rm -Rf ~/Library/Caches
+rm -Rf ~/Library/Application\ Support/brave
+rm -Rf ~/Library/Application\ Support/CallHistory*
+rm -Rf ~/Library/Application\ Support/Google
+rm -Rf ~/Library/Application Support/Transmission 
+rm -Rf ~/.ssh
+rm -Rf ~/Library/Group Containers/com.getdropbox.dropbox.garcon 
+rm -Rf ~/Library/Containers/com.getdropbox.dropbox.garcon 
+rm -Rf ~/.dropbox
+echo "Remember to delete Internet Accounts"
