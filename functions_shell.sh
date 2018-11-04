d95a8405e98282d36c603b83a688547748d18519
Zachary Jones <zak@newalexandria.org>
2018-11-04 16:34:46 -0500

    quick uncompress alias for common archives

--- functions_shell.sh
+++ functions_shell.sh
@@ -7,6 +7,7 @@ function ssh_keygen_fingerprint() {
   ssh-keygen -lv -f $1
 }
 
+
 function find_in_min() {
   find . -type d -name .svn -prune -o -mmin -${1} -type f -print
 }
@@ -18,6 +19,29 @@ function ffrm() {
   find . -name ${1} -depth -exec rm {} \;
 }
 
+
+#   extract:  Extract most know archives with one command
+#   ---------------------------------------------------------
+extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
 
 # find files that are 10MB in size, or larger
 # takes PATH as an argument
