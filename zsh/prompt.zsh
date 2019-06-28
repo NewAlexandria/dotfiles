fa8ce9aacab4758f62506c09db22927c89b77dbe
ZJ <zak@newalexandria.org>
2019-06-28 17:10:20 -0400

    zsh: quick prompt hacks

---
+++ zsh/prompt.zsh
@@ -0,0 +1,33 @@
+function collapse_pwd {
+    echo $(pwd | sed -e "s,^$HOME,~,")
+}
+
+function prompt_char {
+    git branch >/dev/null 2>/dev/null && echo '±' && return
+    hg root >/dev/null 2>/dev/null && echo '☿' && return
+    echo ''
+}
+
+function battery_charge {
+    echo `$BAT_CHARGE` 2>/dev/null
+}
+
+function virtualenv_info {
+    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
+}
+
+
+function hg_prompt_info {
+    hg prompt --angle-brackets "\
+< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
+%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
+patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
+}
+
+PROMPT='
+%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
+%F{202}Ψ%{$reset_color%}$(virtualenv_info)$(prompt_char) '
+
+PROMPT='%F{45}%n%F{250}@%F{3}%m%{$reset_color%}%F{248} in %F{3}%{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%} %F{148}$(hg_prompt_info)$(git_prompt_info)
+%F{202}Ψ%F{248}%{$reset_color%}$(virtualenv_info)$(prompt_char) %F{248}'
