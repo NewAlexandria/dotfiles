3ad47eb484f529e77942c07793f4273d7b7ff70b
ZJ <zak@newalexandria.org>
2020-06-06 21:32:40 -0400

    achrive method of cleaning dot-dirs on S3

--- utils/archive_utils.rb
+++ utils/archive_utils.rb
@@ -18,3 +18,32 @@ file_groups = files.sort.
 mkdirs = file_groups.
   map{|g| Dir.mkdir( File.join(Dir.pwd, g[0])) }
 
+
+## folders
+
+ff = Dir.glob("**/*/")
+ffm = ff.map {|e| [e, e.gsub(' ', '-')] }
+
+## S3 folders
+
+# remove folder/object paths with "./" in them.
+# These are accidental unix dot reference uploads
+def dot_dirs_for( target_bucket ) do
+  require 'open3'
+
+	stdout_str, stderr_str, status = Open3.capture3(
+		"aws s3 ls --recursive #{target_bucket}"
+	)
+	sall = stdout_str.split("\n")
+	dotdirss = sall.
+    map    { |l| l.split(/ [0-9]{1,8} /).map(&:strip) }.
+    select { |l| l.last.match(/.*\.\/.*/) }
+end
+
+threads = []; dotdirs.each_slice(300){|arr|  threads.
+➣➣push( Thread.new { arr.map{|f| stdout_str, stderr_str, status = Open3.
+➣➣capture3("aws s3 rm \"#{target_bucket}#{f.last}\"") ; stdout_str.size }}) }  ;1
+
+threads.map(&:alive?).map(&:to_s).group_by(&:size).map{|e| e.last.size }
+
+
