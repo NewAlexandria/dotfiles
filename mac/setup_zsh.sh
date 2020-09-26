31d16b99decc1d48fd0a3cdcb430048481f6239c
ZJ <zak@newalexandria.org>
2020-09-25 23:44:10 -0400

    zsh: oh-my-zsh links into dotfiles

--- mac/setup_zsh.sh
+++ mac/setup_zsh.sh
@@ -9,3 +9,9 @@ echo
 mkdir ~/.oh-my-zsh/custom/
 cp .zsh-oh-my-zsh/* ~/.oh-my-zsh/custom/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+mv ~/.oh-my-zsh/default.zsh  ~/.oh-my-zsh/default.old.zsh 
+ln -s ~/.dotfiles/.zsh-oh-my-zsh/default.zsh ~/.oh-my-zsh/default.zsh 
+mv ~/.oh-my-zsh/functions.zsh  ~/.oh-my-zsh/functions.old.zsh 
+ln -s ~/.dotfiles/.zsh-oh-my-zsh/functions.zsh ~/.oh-my-zsh/functions.zsh 
+
