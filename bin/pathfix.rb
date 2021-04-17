a8d7a1d028ce119609c6fbace035bc21adb477a6
ZJ <zak@newalexandria.org>
2021-04-17 17:05:45 -0400

    method to swap the order of where brew loads from apple m1 /opt, vs /user/bin

---
+++ bin/pathfix.rb
@@ -0,0 +1,9 @@
+path = ENV['PATH'].split(':')
+
+intel_brew_idx = path.index("/usr/local/bin")
+arm_brew_idx   = path.index("/opt/homebrew/bin")
+if (arm_brew_idx > intel_brew_idx) 
+  ab = path.delete("/opt/homebrew/bin")
+  path.insert(intel_brew_idx, ab) 
+end
+puts path.join(":")
