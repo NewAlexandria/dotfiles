c3383b8f156bca17131b668c571cfdf8b9911ebe
ZJ <zak@newalexandria.org>
2022-02-01 14:48:08 -0500

    typos

--- mac/evac.sh
+++ mac/evac.sh
@@ -1,7 +1,7 @@
 sudo rm -Rf ~/src/projects/pursuit
 
 rm -Rf ~/../equinox/
-rm -Rf /Users/Zachary/dropbox_fp 
+rm -Rf /Users/zak/dropbox_fp 
 rm -Rf ~/dropbox_fp
 rm -Rf ~/google_drive_kf
 rm -Rf ~/src/production/
