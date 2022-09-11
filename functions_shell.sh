5ce49815456960414f53e57c11623ad3c65a0b2a
ZJ <zak@newalexandria.org>
2022-09-11 14:30:07 -0400

    formatting

--- functions_shell.sh
+++ functions_shell.sh
@@ -4,7 +4,7 @@
 # v-naise https://stackoverflow.com/a/246128/263858
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null && pwd)"
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
@@ -12,14 +12,10 @@ done
 DOTFILES_REPO="/Users/$(whoami)/.dotfiles"
 
 function getuser() {
-  if [ -z "$LOGNAME" ]
-  then
-    if [ -z "$USER" ]
-    then
-      if [ -z "$LNAME" ]
-      then
-        if [ -z "$USERNAME" ]
-        then
+  if [ -z "$LOGNAME" ]; then
+    if [ -z "$USER" ]; then
+      if [ -z "$LNAME" ]; then
+        if [ -z "$USERNAME" ]; then
           # do nothing
         else
           echo "$USERNAME"
@@ -53,35 +49,34 @@ alias cd.='cd ~/.ssh/'
 alias cddot='cd ~/.dotfiles/'
 alias cds='cd ~/src/'
 
-alias  ..='cd ../'                           # Go back 1 directory level
-alias ...='cd ../../'                       # Go back 2 directory levels
-alias  .3='cd ../../../'                     # Go back 3 directory levels
-alias  .4='cd ../../../../'                  # Go back 4 directory levels
-alias  .5='cd ../../../../../'               # Go back 5 directory levels
-alias  .6='cd ../../../../../../'            # Go back 6 directory levels
-
+alias ..='cd ../'                # Go back 1 directory level
+alias ...='cd ../../'            # Go back 2 directory levels
+alias .3='cd ../../../'          # Go back 3 directory levels
+alias .4='cd ../../../../'       # Go back 4 directory levels
+alias .5='cd ../../../../../'    # Go back 5 directory levels
+alias .6='cd ../../../../../../' # Go back 6 directory levels
 
 man() {
-	env \
-		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-		LESS_TERMCAP_md=$(printf "\e[1;31m") \
-		LESS_TERMCAP_me=$(printf "\e[0m") \
-		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;32m") \
-			man "$@"
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
 }
 
 ## Finding in Files
 alias ag='ag --color  --color-path=37  --color-line-number=32'
 alias ack='ack --color-lineno=green --color-filename=white --color --follow'
 
-# export GREP_OPTIONS="--color=auto --line-number --context=0 --exclude=*.log" 
-export GREP_COLOR="1;37;41" 
+# export GREP_OPTIONS="--color=auto --line-number --context=0 --exclude=*.log"
+export GREP_COLOR="1;37;41"
 
- # parallel bash commands
- # http://www.rankfocus.com/use-cpu-cores-linux-commands/
+# parallel bash commands
+# http://www.rankfocus.com/use-cpu-cores-linux-commands/
 function pgrep() {
   cat $1 | parallel --block 10M --pipe grep $2
 }
@@ -138,39 +133,41 @@ function ssh_keygen_fingerprint() {
   ssh-keygen -lv -f $1
 }
 
-
 function mvnospace() {
   mv "$1" "${1// /-}"
 }
 
 #   extract:  Extract most know archives with one command
 #   ---------------------------------------------------------
-extract () {
-    if [ -f $1 ] ; then
-      case $1 in
-        *.tar.bz2)   tar xjf $1     ;;
-        *.tar.gz)    tar xzf $1     ;;
-        *.bz2)       bunzip2 $1     ;;
-        *.rar)       unrar e $1     ;;
-        *.gz)        gunzip $1      ;;
-        *.tar)       tar xf $1      ;;
-        *.tbz2)      tar xjf $1     ;;
-        *.tgz)       tar xzf $1     ;;
-        *.zip)       unzip $1       ;;
-        *.Z)         uncompress $1  ;;
-        *.7z)        7z x $1        ;;
-        *)     echo "'$1' cannot be extracted via extract()" ;;
-         esac
-     else
-         echo "'$1' is not a valid file"
-     fi
+extract() {
+  if [ -f $1 ]; then
+    case $1 in
+    *.tar.bz2) tar xjf $1 ;;
+    *.tar.gz) tar xzf $1 ;;
+    *.bz2) bunzip2 $1 ;;
+    *.rar) unrar e $1 ;;
+    *.gz) gunzip $1 ;;
+    *.tar) tar xf $1 ;;
+    *.tbz2) tar xjf $1 ;;
+    *.tgz) tar xzf $1 ;;
+    *.zip) unzip $1 ;;
+    *.Z) uncompress $1 ;;
+    *.7z) 7z x $1 ;;
+    *) echo "'$1' cannot be extracted via extract()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
 }
-
 
 function random() {
   num=$1
   [ ! -z $1 ] || num='12'
-  env LC_CTYPE=C LC_ALL=C tr -dc "a-zA-Z0-9-_\^\&\$\?\(\)\*\#\@\!\%" < /dev/urandom | head -c $num; echo
+  env LC_CTYPE=C LC_ALL=C tr -dc "a-zA-Z0-9-_\^\&\$\?\(\)\*\#\@\!\%" </dev/urandom | head -c $num
+  echo
   #openssl rand -base64 $num
 }
 
+function pghsol() {
+  ruby -e "require 'time'; puts 'Sol ' + (Date.today.mjd - DateTime.parse('2022-08-01').mjd + 1).to_s"
+}
